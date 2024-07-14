4781015504ac9b1b8fc1d5e7d82905c82b075a07
Florian Warninghoff <florian.warninghoff@gmail.com>
2024-07-14 16:42:34 +0200

    lazy load vim visual multi

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -1,3 +1,11 @@
 return {
-  { 'mg979/vim-visual-multi' },
+  {
+    'mg979/vim-visual-multi',
+    keys = {
+      { '<C-n>', desc = 'Multicursor at [N]ext Word' },
+      { '<C-Down>', desc = 'Multicursor below current line' },
+      { '<C-Up>', desc = 'Multicursor above current line' },
+      -- Q - skip current  ],[ move to next / previous
+    },
+  },
 }
