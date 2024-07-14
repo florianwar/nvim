9d7a9329f2b782b71baa15054480c5e9aed56b65
Florian Warninghoff <florian.warninghoff@gmail.com>
2024-07-14 16:26:11 +0200

    add zen mode and ufo

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    'folke/zen-mode.nvim',
+    dependencies = {
+      {
+        'joshuadanpeterson/typewriter',
+        config = function()
+          require('typewriter').setup({
+            enable_with_zen_mode = true,
+            keep_cursor_position = true,
+            enable_notifications = true,
+          })
+        end,
+        opts = {},
+      },
+    },
+    keys = {
+      { '<leader>tz', '<cmd>ZenMode<cr>', { desc = '[T]oggle [Z]en-mode' } },
+    },
+    opts = {
+      window = {
+        backdrop = 0.8,
+        width = 140,
+        height = 1,
+        options = {
+          list = false,
+        },
+      },
+      plugins = {
+        options = {
+          enabled = true,
+          ruler = false,
+          showcmd = false,
+          laststatus = 0,
+        },
+        gitsigns = { enabled = false },
+        kitty = {
+          enabled = true,
+          font = '+2',
+        },
+      },
+      on_open = function()
+        vim.cmd('TWEnable')
+      end,
+      on_close = function()
+        vim.cmd('TWDisable')
+      end,
+    },
+  },
+}
