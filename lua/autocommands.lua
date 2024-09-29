7c2593ef9896ee4e57ddb8047f2e95c7957d20fe
Florian Warninghoff <florian.warninghoff@gmail.com>
2024-09-29 11:33:40 +0200

    some autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -30,3 +30,32 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf })
   end,
 })
+
+-- Dont add comments on newline
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    vim.cmd('set formatoptions-=cro')
+    vim.cmd('setlocal formatoptions-=cro')
+  end,
+})
+
+-- No diagnostics in node modules
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  group = vim.api.nvim_create_augroup('DisableEslintOnNodeModules', { clear = true }),
+  pattern = { '**/node_modules/**', 'node_modules', '/node_modules/*' },
+  callback = function()
+    vim.diagnostic.enable(false)
+  end,
+})
+
+-- Create a dir when saving a file if it doesnt exist
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('auto_create_dir', { clear = true }),
+  callback = function(args)
+    if args.match:match('^%w%w+://') then
+      return
+    end
+    local file = vim.uv.fs_realpath(args.match) or args.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+  end,
+})
