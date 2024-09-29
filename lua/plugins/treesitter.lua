3a720f8524a71c332b35ecfe19411bb5ffbdb34b
Florian Warninghoff <florian.warninghoff@gmail.com>
2024-09-29 12:19:17 +0200

    nice w-e-b motions

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -146,5 +146,16 @@ return {
       use_default_keymaps = false,
       max_join_length = 120,
     },
+    {
+      'chrisgrieser/nvim-spider',
+      opts = {},
+      -- stylua: ignore
+      keys = {
+        { "e", mode = { "n", "o", "x" }, function() require("spider").motion("e") end, desc = "Spider-e" },
+        { "w", mode = { "n", "o", "x" }, function() require("spider").motion("w") end, desc = "Spider-w" },
+        { "b", mode = { "n", "o", "x" }, function() require("spider").motion("b") end, desc = "Spider-b" },
+        { "E", mode = { "n", "x", "o" }, function() require("spider").motion("ge") end, desc = "Spider-E" },
+      },
+    },
   },
 }
